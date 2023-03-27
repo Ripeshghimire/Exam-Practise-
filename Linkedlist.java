d559058ad1a836f5410ebe27c1a13d52a5cd49ee
Ripesh ghimire <ripeshghimire@Ripeshs-MacBook-Air.local>
2023-03-27 20:03:21 +0545

    added changes in the linked list file

--- Linkedlist.java
+++ Linkedlist.java
@@ -54,5 +54,38 @@ class LinkedList{
         head.next = head;
         size--;
     }
-
+    public void deletelast(){
+        if(size == 0){
+            System.out.println("no items to delete in a list ");
+        }else if (size==1){
+            head = tail = null;
+        }
+        node temp = head;
+        for(int i = 0 ; i<=size-1;i++){
+            temp = temp.next;
+        }
+         temp = tail;
+        temp.next = null;
+        size--;
+    }
+    public void deletespecified(int position){
+        if (position<0 || position>=size){
+            System.out.println("invalid arguments");
+        }
+        else if(position==0){
+            deletefirst();
+        }
+        else if(position == size-1){
+            deletelast();    }
+        else{
+            node temp = head;
+            for(int i = 0 ; i<position-1;i++)
+            {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            size--;
+        }
+    }
+   
 }
